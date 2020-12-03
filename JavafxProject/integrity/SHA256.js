6a024e861ecbf2c157ff4f906ceeaab3f77540e7
x18131212 <x18131212@student.ncirl>
2020-12-03 21:38:56 +0000

    Update SHA256.js

--- JavafxProject/integrity/SHA256.js
+++ JavafxProject/integrity/SHA256.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
 
-const secret = 'a1b2c3';
 
-const hash = crypto.createHmac('sha256', secret)
-                   .update('Help me')
+const secret = 'HELLO';
+
+const hash = crypto.createHash("sha256")
+                   .update(secret)
                    .digest('hex');
 
 console.log(hash);
